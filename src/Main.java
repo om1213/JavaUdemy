9e10123ddaa8521705da3f8ebf3209c648ff0d77
om1213 <62217407+om1213@users.noreply.github.com>
2021-05-24 15:43:44 +0530

    Learning partner added

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
 
 
-///Author : Omkar Mapuskar on 24May 2021
+///Author : Omkar Mapuskar on 24May 2021 - Along with Amrutha
 
     public static void main(String[] args) {
        SpeedConversion.checknumber(10);
